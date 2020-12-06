b100325858cdd11c8abfd97cf3f5e66b29f43e56
Dmitry Bakunovich <dv.bakunovich@gmail.com>
2020-12-06 03:26:44 +0300

    Added style sheet for sort menu

--- src/stylehelper/stylehelper.cpp
+++ src/stylehelper/stylehelper.cpp
@@ -1,1 +1,18 @@
-#include "src/stylehelper/stylehelper.h"
+#include "stylehelper.h"
+
+QString StyleHelper::getMenuStyleSheet() {
+    return "QMenu {"
+           "border: none"
+           "}"
+           "QMenu::item {"
+           "padding: 2px 5px 2px 2px;"
+           "}"
+           "QMenu::item:selected {"
+           "background-color: rgb(0, 85, 127);"
+           "color: rgb(255, 255, 255);"
+           "}"
+           "QMenu::item::disabled {"
+           "background-color:black;"
+           "color: white;"
+           "}";
+}
